c9a80da8be7d07b96c8435e56beb54c8b52cd166
lilcheti <farbod@tokhmi.xyz>
2023-08-13 19:28:50 +0330

    gg

---
+++ V2rayNG/app/src/main/kotlin/com/github/bitcoinvpn/dto/ServersCache.kt
@@ -0,0 +1,4 @@
+package com.github.bitcoinvpn.dto
+
+data class ServersCache(val guid: String,
+                        val config: ServerConfig)
---
+++ V2rayNG/app/src/main/kotlin/com/github/bitcoinvpn/service/ServiceControl.kt
@@ -0,0 +1,13 @@
+package com.github.bitcoinvpn.service
+
+import android.app.Service
+
+interface ServiceControl {
+    fun getService(): Service
+
+    fun startService()
+
+    fun stopService()
+
+    fun vpnProtect(socket: Int): Boolean
+}
